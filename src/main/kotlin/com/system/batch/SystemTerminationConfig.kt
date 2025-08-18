47ca82f2061d358df4ca9544cb3a24d0b0cf1dc5
does <ldk980130@gmail.com>
2025-08-19 00:04:22 +0900

    Add SystemTerminationConfig to define steps for batch job simulation

---
+++ src/main/kotlin/com/system/batch/SystemTerminationConfig.kt
@@ -0,0 +1,74 @@
+package com.system.batch
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import java.util.concurrent.atomic.AtomicInteger
+
+@Configuration
+class SystemTerminationConfig(
+    private val jobRepository: JobRepository,
+    private val transactionManager: PlatformTransactionManager,
+) {
+    private val processesKilled = AtomicInteger(0)
+
+    companion object {
+        private const val TERMINATION_TARGET = 5
+    }
+
+    @Bean
+    fun systemTerminationSimulationJob(): Job =
+        JobBuilder("systemTerminationSimulationJob", jobRepository)
+            .start(enterWorldStep())
+            .next(meetNPCStep())
+            .next(defeatProcessStep())
+            .next(completeQuestStep())
+            .build()
+
+    @Bean
+    fun enterWorldStep(): Step =
+        StepBuilder("enterWorldStep", jobRepository)
+            .tasklet({ contribution, chunkContext ->
+                println("System Termination Process is running...")
+                RepeatStatus.FINISHED
+            }, transactionManager)
+            .build()
+
+    @Bean
+    fun meetNPCStep(): Step =
+        StepBuilder("meetNPCStep", jobRepository)
+            .tasklet({ contribution, chunkContext ->
+                println("Meet NPC Process is running...")
+                RepeatStatus.FINISHED
+            }, transactionManager)
+            .build()
+
+    @Bean
+    fun defeatProcessStep(): Step =
+        StepBuilder("defeatProcessStep", jobRepository)
+            .tasklet({ contribution, chunkContext ->
+                val termiated = processesKilled.incrementAndGet()
+                println("Defeat Process is running...(현재 $termiated / $TERMINATION_TARGET)")
+                if (termiated < TERMINATION_TARGET) {
+                    RepeatStatus.CONTINUABLE
+                } else {
+                    RepeatStatus.FINISHED
+                }
+            }, transactionManager)
+            .build()
+
+    @Bean
+    fun completeQuestStep(): Step =
+        StepBuilder("completeQuestStep", jobRepository)
+            .tasklet({ contribution, chunkContext ->
+                println("Complete Quest Process, $TERMINATION_TARGET Process is completed.")
+                RepeatStatus.FINISHED
+            }, transactionManager)
+            .build()
+}
