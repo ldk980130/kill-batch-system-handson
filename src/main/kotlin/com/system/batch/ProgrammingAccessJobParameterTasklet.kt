57896251eade69d81843668e5eacf8074a33172b
does <ldk980130@gmail.com>
2025-08-27 23:27:46 +0900

    Add ProgrammingAccessJobParameterTasklet to handle job parameters dynamically

---
+++ src/main/kotlin/com/system/batch/ProgrammingAccessJobParameterTasklet.kt
@@ -0,0 +1,31 @@
+package com.system.batch
+
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.batch.core.JobParameters
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.stereotype.Component
+
+@Component
+class ProgrammingAccessJobParameterTasklet : Tasklet {
+    private val log: KLogger = KotlinLogging.logger {}
+
+    override fun execute(
+        contribution: StepContribution,
+        chunkContext: ChunkContext,
+    ): RepeatStatus {
+        val jobParameters: JobParameters =
+            chunkContext.stepContext.stepExecution.jobParameters
+
+        val targetSystem = jobParameters.getString("system.target")
+        val destructionLevel: Long = jobParameters.getLong("system.destruction.level")!!
+
+        log.info { targetSystem }
+        log.info { destructionLevel }
+
+        return RepeatStatus.FINISHED
+    }
+}
